b135ed111b03e9d0ed6415d5b433f6cd350ba048
GeoCook <geo_94@live.co.uk>
2019-09-27 15:04:51 +0100

    for the love of mary mother of jesus please work

--- public/index.js
+++ public/index.js
@@ -3,6 +3,11 @@ const button = document.getElementById('button')
 const inputQuery = document.getElementById('inputQuery')
 const summary = document.getElementById('summary')
 const newsTitle = document.getElementById('News-Title')
+const newsAuthor = document.getElementById('News-Author')
+const newsPublished = document.getElementById('News-Published')
+const newsDescr = document.getElementById('News-Description')
+const newsImage = document.getElementById('News-Image')
+const newsLink = document.getElementById('News-Link')
 
 //when button is clicked
 button.addEventListener('click', async ()=>{
@@ -15,9 +20,14 @@ button.addEventListener('click', async ()=>{
     console.log("entered query")
     console.log(`inputQuery.value in index is ${inputQuery.value}`)
     //turning JSON to text
-    let data = await response.text()
+    let data = await response.json()
     console.log("set data")
     console.log(data)
     //let the newsTitle be set the data that has been returned
-    newsTitle.textContent = data
+    newsTitle.textContent = data.title
+    newsAuthor.textContent = data.author
+    newsPublished.textContent = data.published
+    newsDescr.textContent = data.descr
+    newsImage.src = data.imageURL
+    newsLink.textContent = data.articleURL
 })
